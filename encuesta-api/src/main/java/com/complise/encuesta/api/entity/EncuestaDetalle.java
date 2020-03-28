3c7ee261d65262bcebc50dbcd8a05136ce442323
Romeo Arce <Romeo Arce@RARCEP-PC>
2020-03-28 04:10:30 -0600

    Eliminar imports sin uso entitys

--- encuesta-api/src/main/java/com/complise/encuesta/api/entity/EncuestaDetalle.java
+++ encuesta-api/src/main/java/com/complise/encuesta/api/entity/EncuestaDetalle.java
@@ -1,8 +1,6 @@
 package com.complise.encuesta.api.entity;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,7 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "encuesta_detalles")
